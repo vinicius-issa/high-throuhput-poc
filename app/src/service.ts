01f539ba35f3e8cd644365e66878b78d6f8d5e39
Vinicius Issa <vinicius.almeidaissa@gmail.com>
2024-10-17 14:10:48 -0300

    refactor: update validation

--- app/src/service.ts
+++ app/src/service.ts
@@ -1,43 +1,46 @@
 import { Repository, Ticket } from "./repository";
 
 export class Service {
-  private readonly repository: Repository
-  private readonly limit: number
-  
+  private readonly repository: Repository;
+  private readonly limit: number;
+
   constructor(_repository: Repository) {
-    this.repository = new Repository()
+    this.repository = new Repository();
     this.limit = Number(process.env.PUBLIC_LIMIT) || 20_000;
   }
 
-  private async sleep (ms: number) {
-    new Promise(resolve => setTimeout(resolve, ms))
-  } 
-
-  private async generateTicketNumber (): Promise<number> {
-    await this.sleep(500) //some expensive process
-    return Math.floor(Math.random() * 1_000_000)
+  private async sleep(ms: number) {
+    new Promise((resolve) => setTimeout(resolve, ms));
   }
 
-  private async validateTicketLimit (): Promise<void> {
-    const totalSold = await this.repository.countTickets()
-    if(totalSold >= this.limit) {
-      throw new Error("No ticket enough")
+  private async generateTicketNumber(): Promise<number> {
+    return Math.floor(Math.random() * 1_000_000);
+  }
+
+  private async validateTicketLimit(): Promise<void> {
+    const totalSold = await this.repository.countTickets();
+    if (totalSold >= this.limit) {
+      throw new Error("No ticket enough");
     }
   }
 
-  public async sellTicket(ticket: { userId: string, eventId: string }): Promise<Ticket> {
-    const ticketNumber = await this.generateTicketNumber()
-    
-    const existTicket = await this.repository.existTicket(ticketNumber)
-    if(existTicket) {
-      throw new Error("TicketId exist")
-      }
-      
+  public async sellTicket(ticket: {
+    userId: string;
+    eventId: string;
+  }): Promise<Ticket> {
+    await this.sleep(500); //some expensive process
+    const ticketNumber = await this.generateTicketNumber();
+
+    const existTicket = await this.repository.existTicket(ticketNumber);
+    if (existTicket) {
+      throw new Error("TicketId exist");
+    }
+
     const ticketSelled = {
       ...ticket,
       ticketNumber,
-      };
-      
+    };
+
     await this.validateTicketLimit();
     await this.repository.insertTicket(ticketSelled);
     return ticketSelled;
